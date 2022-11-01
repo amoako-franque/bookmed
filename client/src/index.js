27c85108a22877848c8c99bf67ce2e29b68c0ab0
armoako <frank.amoako-dev@outlook.com>
2022-11-01 21:20:36 +0000

    minor fix

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import 'antd/dist/antd.min.css'
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./styles/index.css";
 import { Provider } from "react-redux";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,31 +1,32 @@
-import { Button, Form, Input } from "antd";
-import React from "react";
-import toast from "react-hot-toast";
-import { useSelector, useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import { hideLoading, showLoading } from "../redux/alertsSlice";
+import { Button, Form, Input } from "antd"
+import React from "react"
+import toast from "react-hot-toast"
+import { useDispatch } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
+import axios from "axios"
+import { hideLoading, showLoading } from "../redux/alertsSlice"
 
 function Login() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
   const onFinish = async (values) => {
     try {
-      dispatch(showLoading());
-      const response = await axios.post("/api/user/login", values);
-      dispatch(hideLoading());
+      dispatch(showLoading())
+      const response = await axios.post("/api/user/login", values)
+      dispatch(hideLoading())
       if (response.data.success) {
-        toast.success(response.data.message);
-        localStorage.setItem("token", response.data.data);
-        navigate("/");
+        toast.success(response.data.message)
+        localStorage.setItem("token", response.data.data)
+        navigate("/")
       } else {
-        toast.error(response.data.message);
+        toast.error(response.data.message)
       }
     } catch (error) {
-      dispatch(hideLoading());
-      toast.error("Something went wrong");
+      dispatch(hideLoading())
+      toast.error("Something went wrong")
     }
-  };
+  }
 
   return (
     <div className="authentication">
@@ -39,7 +40,7 @@ function Login() {
             <Input placeholder="Password" type="password" />
           </Form.Item>
 
-          
+
           <Button className="primary-button my-2 full-width-button" htmlType="submit">
             LOGIN
           </Button>
@@ -47,11 +48,11 @@ function Login() {
           <Link to="/register" className="anchor mt-2">
             CLICK HERE TO REGISTER
           </Link>
-         
+
         </Form>
       </div>
     </div>
-  );
+  )
 }
 
-export default Login;
+export default Login
